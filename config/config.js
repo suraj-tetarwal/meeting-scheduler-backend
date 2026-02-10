41e56918accf453e1aec07006d17e74404af1399
Suraj Tetarwal <lord.skt210503@gmail.com>
2026-02-10 21:23:48 +0530

    Run migrations on startup

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ require("dotenv").config()
 module.exports = {
   development: {
     dialect: "sqlite",
-    storage: process.env.DB_STORAGE,
-  }
-}
+    storage: process.env.DB_STORAGE || "database.sqlite",
+  },
+};
