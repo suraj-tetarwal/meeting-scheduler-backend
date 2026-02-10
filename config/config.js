a9f154be44bde65af3a0ae8099bfbcd8c2dc189f
Suraj Tetarwal <lord.skt210503@gmail.com>
2026-02-10 21:28:05 +0530

    Add production config for sqlite migrations

--- config/config.js
+++ config/config.js
@@ -1,7 +1,11 @@
-require("dotenv").config()
+require("dotenv").config();
 
 module.exports = {
   development: {
+    dialect: "sqlite",
+    storage: process.env.DB_STORAGE || "database.sqlite",
+  },
+  production: {
     dialect: "sqlite",
     storage: process.env.DB_STORAGE || "database.sqlite",
   },
